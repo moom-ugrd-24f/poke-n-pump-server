e2253870c33f8ad197a95e267b40bce80a4a320f
Sejun Jung <sejunisbest@kaist.ac.kr>
2024-11-06 20:35:22 +0900

    Initial Setup

---
+++ models/FriendRequest.js
@@ -0,0 +1,28 @@
+// models/FriendRequest.js
+const mongoose = require('mongoose');
+
+const friendRequestSchema = new mongoose.Schema({
+  senderId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  receiverId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['pending', 'accepted'],
+    default: 'pending',
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const FriendRequest = mongoose.model('FriendRequest', friendRequestSchema);
+
+module.exports = FriendRequest;
---
+++ models/User.js
@@ -0,0 +1,59 @@
+// models/User.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  nickname: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  inviteCode: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  xp: {
+    type: Number,
+    default: 0,
+  },
+  profilePicture: {
+    type: String,
+    default: 'default-profile.jpg', // 프로필 사진 기본값 설정
+  },
+  shamePostSettings: {
+    isEnabled: {
+      type: Boolean,
+      default: false,
+    },
+    noGymStreakLimit: {
+      type: Number,
+      default: 5,
+    },
+  },
+  workoutPlan: {
+    daysOfWeek: {
+      type: [Number], 
+      default: [], 
+    },
+  },
+  todayAttendance: {
+    type: Boolean,
+    default: false,
+  },
+  noGymStreak: {
+    type: Number,
+    default: 0,
+  },
+  friends: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+}, {
+  timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
