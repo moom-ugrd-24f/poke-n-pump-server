d30783406b274fddad309c6373ba61a0e47b069a
Sejun Jung <sejunisbest@kaist.ac.kr>
2024-11-20 19:31:32 +0900

    struct2

--- utils/generateInviteCode.js
+++
@@ -1,9 +0,0 @@
-// utils/generateInviteCode.js
-
-async function generateInviteCode() {
-    const { customAlphabet } = await import('nanoid');
-    const nanoid = customAlphabet('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', 6);
-    return nanoid();
-  }
-
-module.exports = generateInviteCode;
